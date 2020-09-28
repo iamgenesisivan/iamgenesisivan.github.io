4b66c7926363982295ff451c420528586760ea81
MONSTEEEER <aquino.igenesis@gmail.com>
2020-09-28 13:35:08 +0800

    remove spaces
    remove spaces

--- assets/js/typetext.js
+++ assets/js/typetext.js
@@ -1,11 +1,6 @@
 var content = "Hi, I'm Genesis Ivan Aquino."
-
 var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
-
-
 $(ele).hide().appendTo('.intro-text').each(function (i) {
-
-
     $(this).delay(100 * i)
     .queue(function(next) {
     	if (i+1 === content.length) {
@@ -19,17 +14,10 @@ $(ele).hide().appendTo('.intro-text').each(function (i) {
     }).animate({
         opacity: 1
     }, 100);
-
 });
-
-
 function sub_text(){
-
 	var content_2 = "A Web and Software Developer."
-
 	var ele_2 = '<span>' + content_2.split('').join('</span><span>') + '</span>';
-
-
 	$(ele_2).hide().appendTo('.basic-text').each(function (i) {
 	    $(this).delay(100 * i).css({
 	        display: 'inline',
